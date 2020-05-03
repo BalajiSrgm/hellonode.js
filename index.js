e0c3119970d7e41c63d2e29bef17767ef654f767
BalajiShan <balaji.shanmugam@intellectdesign.com>
2020-05-03 07:37:45 +0530

    config added

--- index.js
+++ index.js
@@ -35,5 +35,5 @@ mongoose
   .catch(err => console.log(err));
   mongoose.set('debug', true);
 
-app.listen(4000);
+app.listen(process.env.APP_PORT || 4000);
 console.log('Running a GraphQL API server at localhost:4000/graphql');

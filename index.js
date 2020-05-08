56036d1b1820f1ec14780bbeef892c886752d020
BalajiShan <balaji.shanmugam@intellectdesign.com>
2020-05-08 15:52:51 +0530

    chore: lint aligned

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 
 require('dotenv').config()
 
-var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
-
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const { buildSchema, execute, subscribe } = require('graphql')
+const { SubscriptionServer } = require('subscriptions-transport-ws')
+const cors = require('cors')
+const compression = require('compression')
+const { createServer } = require('http')
 // import mongoose from "mongoose";
-const mongoose = require('mongoose');
-const typeDefs = require('./src/graphql/typeDefs');
+const mongoose = require('mongoose')
+const typeDefs = require('./src/graphql/typeDefs')
 
 // import typeDefs from './src/graphql/typeDefs';
-const rootValue = require('./src/graphql/resolvers');
+const rootValue = require('./src/graphql/resolvers')
 
-const schema = buildSchema(typeDefs.default);
-let db;
-if(process.env.MG_USER) {
-  db = `mongodb://${process.env.MG_USER}:${process.env.MG_PWD}@${process.env.MG_HOST}:${process.env.MG_PORT}/${process.env.MG_DB_NAME}`;
-}
-else db = "mongodb://localhost:".concat(process.env.MG_PORT, "/local");
-const app = express();
+const schema = buildSchema(typeDefs.default)
+
+const app = express()
 app.use('/graphql', graphqlHTTP({
   schema,
   rootValue: rootValue.default,
-  graphiql: true,
-}));
+  graphiql: true
+}))
+app.use(express.json())
+app.use(cors())
+app.use(compression())
+const server = createServer(app)
+
+let db
+if (process.env.MG_USER) {
+  db = `mongodb://${process.env.MG_USER}:${process.env.MG_PWD}@${process.env.MG_HOST}:${process.env.MG_PORT}/${process.env.MG_DB_NAME}`
+} else db = 'mongodb://localhost:'.concat(process.env.MG_PORT, '/local')
 
 // Connect to MongoDB with Mongoose.
 mongoose
@@ -35,9 +43,24 @@ mongoose
       useUnifiedTopology: true
     }
   )
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
-  mongoose.set('debug', true);
-app.listen(process.env.APP_PORT || 4000);
-console.log('<><>db', db);
-console.log(`Running a GraphQL API server at localhost:${process.env.APP_PORT || 4000}/graphql`);
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err))
+mongoose.set('debug', true)
+
+const onConnect = function onConnect (ab, webSocket) {
+  console.log('ab', ab)
+  console.log('<><>webSocket', webSocket)
+}
+app.listen(process.env.APP_PORT || 4000, () => {
+  new SubscriptionServer({
+    execute,
+    subscribe,
+    onConnect,
+    schema
+  }, {
+    server: server,
+    path: '/subscriptions'
+  })
+})
+console.log('<><>db', db)
+console.log(`Running a GraphQL API server at localhost:${process.env.APP_PORT || 4000}/graphql`)
--- src/graphql/resolvers/userResolver.js
+++ src/graphql/resolvers/userResolver.js
@@ -1,61 +1,81 @@
 
-import User from "../../models/User";
+import User from '../../models/User'
+import { PubSub, withFilter } from 'graphql-subscriptions'
+
+const pubsub = new PubSub()
 
 export default {
-    user: (root, args) => {
-        return new Promise((resolve, reject) => {
-          User.findOne(root).exec((err, res) => {
-            err ? reject(err) : resolve(res);
-          });
-        });
-      },
-      users: () => {
-        return new Promise((resolve, reject) => {
-          User.find({})
-            .populate()
-            .exec((err, res) => {
-              err ? reject(err) : resolve(res);
-            });
-        });
-      },
+  user: (root, args) => {
+    return new Promise((resolve, reject) => {
+      User.findOne(root).exec((err, res) => {
+        err ? reject(err) : resolve(res)
+      })
+    })
+  },
+  users: () => {
+    return new Promise((resolve, reject) => {
+      User.find({})
+        .populate()
+        .exec((err, res) => {
+          err ? reject(err) : resolve(res)
+        })
+    })
+  },
   addUser: (root, { id, name, email }) => {
-    const newUser = new User(root);
+    const newUser = new User(root)
 
     return new Promise((resolve, reject) => {
       newUser.save((err, res) => {
-        err ? reject(err) : resolve(res);
-      });
-    });
+        if (err) reject(err)
+        pubsub.publish('user', { user: res })
+        resolve(res)
+      })
+    })
   },
   editUser: (root, { id, name, email }) => {
-    console.log('<><>Args', root);
+    console.log('<><>Args', root)
     return new Promise((resolve, reject) => {
       User.findOneAndUpdate({ id }, { $set: { name, email } }).exec(
         (err, res) => {
-          err ? reject(err) : resolve(res);
+          err ? reject(err) : resolve(res)
         }
-      );
-    });
+      )
+    })
   },
   deleteUser: (root, args) => {
     return new Promise((resolve, reject) => {
       User.findOneAndRemove(args).exec((err, res) => {
-        err ? reject(err) : resolve(res);
-      });
-    });
+        err ? reject(err) : resolve(res)
+      })
+    })
   },
-  newUser:(root, args) => {
-      return '';
+  newUser: {
+    subscribe: withFilter(() => pubsub.asyncIterator('user'), (res, variables) => {
+      console.log('<><>variables', variables)
+      console.log('res', res.user)
+      return res.user.id === variables.id
+    })
+    // pubsub.subscribe( withFilter(() => pubsub.asyncIterator('user'), (res, variables) => {
+    //   console.log('<><>variables', variables);
+    //   console.log('res', res.user);
+    //   return  res.user.id === variables.id;
+    // }) )
+    // pubsub.subscribe('user', (res) => {
+    //   const a = pubsub.asyncIterator('user');
+    //   console.log('<><>A', a);
+    //   console.log('res', res.user);
+    //   return res.user;
+    // })
   },
   User: {
-    id(user) {
-        return user.id;
+    id (user) {
+      return user.id
     },
-    name(user) {
-        return user.name;
+    name (user) {
+      return user.name
     },
-    email(user) {
-        return user.email;
-    },
+    email (user) {
+      return user.email
+    }
   }
-};
+}
